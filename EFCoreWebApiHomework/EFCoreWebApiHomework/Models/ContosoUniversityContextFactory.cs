6600e5da2ce5ef92c5127caf5378ebcf22243dd1
johnny <johnny@wantgoo.com>
2019-12-09 13:14:19 +0800

    create Project -> Add-Migration Init: 5 mins

---
+++ EFCoreWebApiHomework/EFCoreWebApiHomework/Models/ContosoUniversityContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCoreWebApiHomework.Models
+{
+    public class ContosoUniversityContextFactory : IDesignTimeDbContextFactory<ContosoUniversityContext>
+    {
+        public ContosoUniversityContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ContosoUniversityContext>().UseSqlServer(
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ContosoUniversity;Integrated Security=True");
+
+            return new ContosoUniversityContext(optionsBuilder.Options);
+        }
+    }
+}
